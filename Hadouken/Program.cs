a8d1c3b21b11c63ea2982adcfea8e1f191a5a332
Adam Stirtan <adamstirtan@gmail.com>
2018-06-20 15:03:03 -0400

    Initial commit

---
+++ Hadouken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hadouken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
