5861b1e32c256a5cc8ff016cc5b0b40bc1d4d16d
Adam Stirtan <adamstirtan@gmail.com>
2018-06-21 16:20:16 -0400

    Syntax fix, blaming editing on iPad

--- Hadouken/Commands/YellBarfCommand.cs
+++ Hadouken/Commands/YellBarfCommand.cs
@@ -8,7 +8,7 @@ namespace Hadouken.Commands
 		
 		public void Action(IBot bot, string channel, string args)
 		{
-			bot.Client.SendMessage($"{args.ToUpper()}!!, channel);
+			bot.Client.SendMessage($"{args.ToUpper()}!!", channel);
 		}
 	}
 }
