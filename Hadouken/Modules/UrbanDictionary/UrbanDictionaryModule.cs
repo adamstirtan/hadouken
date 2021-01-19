5a2252ebcb555860608aeacca89c1c0660e963d3
Adam Stirtan <adam.stirtan@fernsoftware.com>
2021-01-18 22:03:57 -0500

    Added !ud trigger

---
+++ Hadouken/Modules/UrbanDictionary/UrbanDictionaryModule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Discord.Commands;
+using Discord;
+
+namespace Hadouken.Modules.UrbanDictionary
+{
+    public class UrbanDictionaryModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger<UrbanDictionaryModule> _logger;
+        private readonly HttpClient _httpClient;
+
+        public UrbanDictionaryModule(ILogger<UrbanDictionaryModule> logger)
+        {
+            _logger = logger;
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.urbandictionary.com/v0/"),
+                Timeout = TimeSpan.FromSeconds(10)
+            };
+        }
+
+        [Command("ud")]
+        [Summary("Define a term using urbandictionary.com. Usage: !ud <term>")]
+        public async Task DefineAsync(string term)
+        {
+            _logger.LogInformation($"Attempting urban dictionary search: {term}");
+
+            if (string.IsNullOrEmpty(term))
+            {
+                await ReplyAsync($"Usage: !ud <term>");
+                return;
+            }
+
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<UrbanDictionaryResponseDto>($"define?term={term}");
+
+                if (response.List.Any())
+                {
+                    var definition = response.List.First();
+
+                    var builder = new EmbedBuilder()
+                        .WithColor(new Color(54, 160, 244))
+                        .WithTitle(definition.Word)
+                        .WithUrl(definition.Permalink)
+                        .WithDescription(definition.Definition)
+                        .WithFooter(definition.Example, "https://freepps.top/images/uploads/soft/2016-12-23/urban-dictionary-material-logo-fit-wX-150.png");
+
+                    await ReplyAsync(embed: builder.Build());
+                }
+                else
+                {
+                    await ReplyAsync($"There's no definition for \"{term}\" on urbandictionary.com");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ Hadouken/Modules/UrbanDictionary/UrbanDictionaryResponseDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Hadouken.Modules.UrbanDictionary
+{
+    internal class UrbanDictionaryResponseDto
+    {
+        [JsonPropertyName("list")]
+        public UrabnDictionaryDefinitionDto[] List { get; set; }
+    }
+
+    internal class UrabnDictionaryDefinitionDto
+    {
+        [JsonPropertyName("defid")]
+        public long DefinitionId { get; set; }
+
+        [JsonPropertyName("definition")]
+        public string Definition { get; set; }
+
+        [JsonPropertyName("permalink")]
+        public string Permalink { get; set; }
+
+        [JsonPropertyName("thumbs_up")]
+        public int ThumbsUp { get; set; }
+
+        [JsonPropertyName("thumbs_down")]
+        public int ThumbsDown { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("word")]
+        public string Word { get; set; }
+
+        [JsonPropertyName("written_on")]
+        public DateTime Created { get; set; }
+
+        [JsonPropertyName("example")]
+        public string Example { get; set; }
+    }
+}
