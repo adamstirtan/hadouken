cbb861113c5104ea28e317668aa017ec99a2e507
Adam Stirtan <adamstirtan@gmail.com>
2021-01-23 02:08:10 +0000

    !vape30

---
+++ Hadouken/Modules/VapeModule/VapeModule.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Discord.Commands;
+
+namespace Hadouken.Modules.Quotes
+{
+    public class VapeModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger<VapeModule> _logger;
+
+        public VapeModule(
+            ILogger<VapeModule> logger)
+        {
+            _logger = logger;
+        }
+
+        // [Command("vape30")]
+        // [Summary("It's VAPE:30 boys")]
+        // public Task VapeAsync()
+        // {
+        //     return Task.CompletedTask;
+        // }
+    }
+}
